d02c2ea7da2e464dba8ac10389ffad597c6b19b2
GiaBao <duonggiabao254@gmail.com>
2025-10-17 21:27:30 +0700

    fix: add middleware to parse JSON and URL-encoded requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// parse request to body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
