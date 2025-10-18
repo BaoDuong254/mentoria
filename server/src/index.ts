a9e28a2a504f4472415af166a3134fb3b0cbdaaf
GiaBao <duonggiabao254@gmail.com>
2025-10-18 19:17:49 +0700

    feat: add logger configuration with Winston and integrate Morgan for request logging - update tsconfig.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,21 @@
+import logger from "@/utils/logger";
 import express from "express";
+import morgan from "morgan";
 const app = express();
 const port = 3000;
 
 // parse request to body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// setup morgan with winston
+app.use(
+  morgan("combined", {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
