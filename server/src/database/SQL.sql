4f60374e88f971d02c90c2cc1beed6cfa9535006
GiaBao <duonggiabao254@gmail.com>
2025-10-21 00:35:18 +0700

    feat: add sql

---
+++ server/src/database/SQL.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+    user_id INT IDENTITY(1,1) PRIMARY KEY,
+    first_name NVARCHAR(50) NOT NULL,
+    last_name NVARCHAR(50) NOT NULL,
+    sex NVARCHAR(10) CHECK (sex IN (N'Male', N'Female')),
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME NULL,
+    email NVARCHAR(100) UNIQUE NOT NULL,
+    password NVARCHAR(255) NOT NULL,
+    avatar_url NVARCHAR(255) NULL,
+    country NVARCHAR(100) NULL,
+    role NVARCHAR(50) CHECK (role IN (N'Mentee', N'Mentor', N'Admin')) DEFAULT N'Mentee',
+    timezone NVARCHAR(50) NULL,
+    status NVARCHAR(20) CHECK (status IN (N'active', N'inactive', N'banned', N'pending')) DEFAULT N'active'
+);
