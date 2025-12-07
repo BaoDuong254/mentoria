12115cff08e79038d269a117199fab3d44d7f53d
GiaBao <duonggiabao254@gmail.com>
2025-12-08 02:32:09 +0700

    fix(db): ensure database creation only occurs if it doesn't already exist

--- server/init-db.sh
+++ server/init-db.sh
@@ -5,7 +5,7 @@ sleep 30s
 
 # Run SQL scripts in order
 echo "Executing SQL.sql to create database schema..."
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U $DB_USER -P $DB_PASS -d master -i /docker-entrypoint-initdb.d/SQL.sql -C -No
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U $DB_USER -P $DB_PASS -d $DB_NAME -i /docker-entrypoint-initdb.d/SQL.sql -C -No
 
 echo "Executing INSERT_DATA.sql to insert sample data..."
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U $DB_USER -P $DB_PASS -d $DB_NAME -i /docker-entrypoint-initdb.d/INSERT_DATA.sql -C -No
